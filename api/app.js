3b44ecc8789bf03c24fdfffbf93aaefa6a8acb79
1732110347 <1732110347@qq.com>
2022-04-15 20:10:12 +0800

    4.15

---
+++ api/app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const joi = require('joi')
+// 导入 cors 中间件
+const cors = require('cors')
+// 将 cors 注册为全局中间件
+app.use(cors())
+
+
+app.use(express.urlencoded({ extended: false }))
+
+// 响应数据的中间件
+app.use(function (req, res, next) {
+    // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
+    res.cc = function (err, status = 1) {
+        res.send({
+            // 状态
+            status,
+            // 状态描述，判断 err 是 错误对象 还是 字符串
+            message: err instanceof Error ? err.message : err,
+        })
+    }
+    next()
+})
+
+
+const expressJWT = require('express-jwt')
+const config = require('./config')
+app.use(expressJWT({
+secret:config.jwtSecretKey
+}).unless({path: [/^\/api/] }))
+
+
+// 导入并注册用户路由模块
+const userRouter = require('./router/user')
+app.use('/api', userRouter)
+
+
+const userinfoRouter =require('./router/userinfo')
+app.use('/my',userinfoRouter)
+
+
+
+app.use((err, req, res, next) => {
+    // 验证失败导致的错误
+    if (err instanceof joi.ValidationError) return res.cc(err)
+
+    if (err.name === 'UnauthorizedError') return res.cc('身份认证失败！')
+    // 未知的错误
+    res.cc(err)
+})
+
+app.listen(3007, () => {
+    console.log('http://127.0.0.1:3007');
+})
