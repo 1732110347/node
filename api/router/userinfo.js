0a8cd952bc3bd575bcb6d680f48fbc204c1c3eb5
1732110347 <1732110347@qq.com>
2022-04-16 19:04:25 +0800

    4.16

--- api/router/userinfo.js
+++ api/router/userinfo.js
@@ -5,6 +5,18 @@ const router = express.Router()
 const userinfo_handler = require('../router_handler/userinfo')
 
 
+// 导入验证数据合法性的中间件
+const expressJoi = require('@escook/express-joi')
+
+// 导入需要的验证规则对象
+const { update_userinfo_schema } = require('../schema/user')
+
+
 router.get('/userinfo', userinfo_handler.getUserInfo)
+
+
+// 更新用户的基本信息
+router.post('/userinfo', expressJoi(update_userinfo_schema), userinfo_handler.updateUserInfo)
+
 
 module.exports = router
